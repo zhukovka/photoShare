e50d2f9fa5aeba4b8c9923039c7556caa94d9b18
zhukovka <helen.zhukova@gmail.com>
2018-06-29 12:07:49 +0300

    mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const { GraphQLServer } = require('graphql-yoga');
+const {GraphQLServer} = require('graphql-yoga');
+const {MongoClient} = require('mongodb')
 
 const typeDefs = `
     type Query {
@@ -12,20 +13,36 @@ const resolvers = {
     }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-
-var port = 4000
-
-server.express.get('/', (req, res) => {
-    res.end(`The PhotoShare Service - http://localhost:${port}/playground`)
-})
+const port = 4000;
 
 const options = {
     port,
     endpoint: '/graphql',
     playground: '/playground'
+};
+
+const ready = ({port}) => console.log(`graph service running - http://localhost:${port}`)
+
+// 1. Create Asynchronous Function
+async function start () {
+
+    // 2. Connect to the database (asynchronous operation)
+    const MONGO_DB = 'mongodb://localhost:27017/graphql-photos';
+    const client = await MongoClient.connect(MONGO_DB);
+    const db = client.db();
+
+    // 3. Once connected, Add the database to context
+    const context = {db};
+
+    // 4. Start the server and send the context object
+    const server = new GraphQLServer({typeDefs, resolvers, context});
+
+    server.express.get('/', (req, res) => {
+        res.end(`The PhotoShare Service - http://localhost:${port}/playground`)
+    });
+
+    server.start(options, ready);
 }
 
-const ready = ({ port }) => console.log(`graph service running - http://localhost:${port}`)
-
-server.start(options, ready)
+// 5. Invoke start when ready to start
+start();
